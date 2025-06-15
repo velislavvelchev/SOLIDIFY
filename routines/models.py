3e5dde07e9b7c1b44f3cdf8e562fab80cbcb8909
velislavvelchev <v.velchev98@gmail.com>
2025-06-15 13:18:08 +0300

    feat: static/dynamic version of all html templates

--- routines/models.py
+++ routines/models.py
@@ -1,7 +1,16 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
 class Routine(models.Model):
+	routine_name = models.CharField(
+		unique=True,
+		max_length=30,
+		validators=[
+			MinLengthValidator(3, message="The routine name must be at least 3 characters long."),
+			#TODO custom validator that checks for characters only
+		]
+	)
 	category = models.ForeignKey(
 		to ='categories.Category',
 		on_delete = models.CASCADE,
