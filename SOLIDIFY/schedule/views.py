732c4598a20dec2c68592c8dcc38f9b8b4af9f36
velislavvelchev <v.velchev98@gmail.com>
2025-07-02 16:39:10 +0300

    feat: timezone conversion feature on create schedule

--- SOLIDIFY/schedule/views.py
+++ SOLIDIFY/schedule/views.py
@@ -1,7 +1,10 @@
+import datetime
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.utils import timezone
+from django.utils.dateparse import parse_datetime
 from django.views import View
 
 from .forms import ScheduleRoutineCreateForm
@@ -22,7 +25,7 @@ class CalendarEventView(View):
                 "title": s.routine.routine_name,
                 "start": s.scheduled_time.isoformat(),
                 "id": s.id,
-            }
+                                            }
             for s in scheduled_routines
         ]
         return JsonResponse(events, safe=False)
@@ -41,8 +44,19 @@ class ScheduleRoutineCreateView(LoginRequiredMixin, CreateView):
     template_name = 'schedule/create_schedule.html'
     success_url = reverse_lazy('calendar')
 
+
     def form_valid(self, form):
         scheduled = form.save(commit=False)
+
+        # Get the hidden UTC field from POST
+        utc_string = self.request.POST.get('scheduled_time_utc')
+        dt_utc = parse_datetime(utc_string) if utc_string else None
+
+        # If we have a value, use it
+        # ... inside your view
+        if dt_utc is not None:
+            dt_utc = timezone.make_aware(dt_utc, datetime.timezone.utc)
+            scheduled.scheduled_time = dt_utc
 
         # Validate time is not in the past
         if scheduled.scheduled_time < timezone.now():
